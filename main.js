a8a32b78a955fd83b09b183082d8644a16925896
listen <13631624455@163.com>
2025-07-08 22:37:39 +0800

    update

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 import Vue from './src/Vue.js';
-import htmlParser from './src/nodes/htmlParse.js';
 
 const app = new Vue({
     el: '#app',
     data: {
+        count: 0,
         message: 'Hello, Vue!',
+        href: './?' + Date.now(),
+        number: 33,
         title: 'This is a title',
         myTitle: 'This is a aaa aaa bb ',
         sss: 'This is a sss',
         checkbox: false,
-        select: 'option1',
+        select: '223',
         radio: 'option1',
+        text: 'just a simple text',
+        color: '#f3c'
     },
     methods: {
+        add() {
+            this.$data.count++;
+        },
+        reduce() {
+            this.$data.count--;
+        },
         changeMessage() {
             if (this.$data.message === 'Message changed!') {
                 this.$data.message = 'Hello, Vue!';
@@ -22,6 +32,7 @@ const app = new Vue({
         },
         onChange(e) {
             console.log('onChange', e);
-        }
+        },
+
     }
 });
